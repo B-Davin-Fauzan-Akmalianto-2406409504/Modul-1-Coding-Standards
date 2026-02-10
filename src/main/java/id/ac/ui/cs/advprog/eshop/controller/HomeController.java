26d2c2bd3558c43c4d8e17d074d9748e11610ead
Narwhll <davin.fauzaan@gmail.com>
2026-02-10 13:54:47 +0700

    functional test for homepage and title is done

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("pageTitle", "NarwhllShop");
+        return "homepage";
+    }
+}
